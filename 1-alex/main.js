013c055580d723e36a412b13fc24c120119f61df
Bosshans442 <BossHans>
2022-12-17 02:31:47 +0000

    great

---
+++ 1-alex/main.js
@@ -0,0 +1,14 @@
+#include "stdlib"
+#include "stdio"
+/**
+ * A number that is divisible by 30 and is an odd number
+ *
+ *Return_ An output number less than 10
+ */
+{ 
+	int main(void)
+	if ("number is less than 20");
+	return(yes);
+	else
+		return(none)
+}
